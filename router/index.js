c7d6ee9d1c098356767b1c2883c55917d24230ff
liuzy <yjwkhhbj@163.com>
2017-09-29 23:11:15 +0800

    up

--- router/index.js
+++ router/index.js
@@ -7,18 +7,33 @@ router.get('/', function(req, res, next) {
 	res.render('index')
 })
 
+function orderId() {
+	return txnTime() + ('00' + new Date().getSeconds()).slice(-3)
+}
+
+function txnTime() {
+	const d = new Date()
+	return [d.getFullYear(),
+		('0' + (d.getMonth() + 1)).slice(-2),
+		('0' + d.getDate()).slice(-2),
+		('0' + d.getHours()).slice(-2),
+		('0' + d.getMinutes()).slice(-2),
+		('0' + d.getSeconds()).slice(-2)
+	].join('')
+}
+
 router.all('/jlwt', function(req, res, next) {
 	API.jlwt({
 		merId: '777290058152112',
-		orderId: '20170929185307599',
-		txnTime: '20170929185307',
+		orderId: orderId(),
+		txnTime: txnTime(),
 		accNo: '6216261000000000018',
-		certifTp:'01',
-		certifId:'341126197709218366',
-		customerNm:'全渠道',
-		phoneNo:'13552535506',
-		cvn2:'',
-		expired:''
+		certifTp: '01',
+		certifId: '341126197709218366',
+		customerNm: '全渠道',
+		phoneNo: '13552535506',
+		cvn2: '',
+		expired: ''
 	}, function(ret) {
 		res.json(ret)
 	})
